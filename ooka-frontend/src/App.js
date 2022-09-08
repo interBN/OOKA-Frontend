2a1ce0fe4fcfa5904b80d43af4f9c52dece2abd9
Siu Cheng <93086542+interBN@users.noreply.github.com>
2022-09-08 15:07:55 +0200

    move todo stuff in new folder

--- ooka-frontend/src/App.js
+++ ooka-frontend/src/App.js
@@ -1,55 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './App.css';
-import TodoList from './TodoList';
-import { v4 as uuidv4 } from 'uuid';
+import { Typography } from '@mui/material';
+import React from 'react';
 
-
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (setTodos) setTodos(storedTodos)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
-
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodo => {
-      return [...prevTodo, { id: uuidv4(), name: name, complete: false }]
-    })
-    todoNameRef.current.value = null
-  }
-
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
-
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
-
-  return (
-    <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add</button>
-      <button onClick={handleClearTodos}>Clear</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
-
-  )
+    return (
+        <div className='App'>
+            <Typography gutterBottom variant="h3" align='center'>
+                React App
+            </Typography>
+        </div>
+    )
 }
 
 export default App;
---
+++ ooka-frontend/src/TODO.md
@@ -0,0 +1,16 @@
+Product contains:
+
+    private String startingSystem;
+    private String auxiliaryPTO;
+    private String oilSystem;
+    private String fuelSystem;
+    private String coolingSystem;
+    private String exhaustSystem;
+    private String mountingSystem;
+    private String engineManagementSystem;
+    private String monitoringSystem;
+    private String powerTransmission;
+    private String gearbox;
+    
+    private int result;
+    private int hash;
